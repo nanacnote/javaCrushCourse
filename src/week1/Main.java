401069a4cbda17702d22d86a668f410c99e1a63f
Nana C-Note <nana@Nanas-MacBook-Pro.local>
2020-10-02 14:08:42 +0100

    week 1 commit

---
+++ src/week1/Main.java
@@ -0,0 +1,19 @@
+package week1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("\n" +
+                "Recipe : Chocolate chip cookies (makes 12 cookies)\n" +
+                "Ingredients: 4oz sugar\n" +
+                "             4oz butter\n" +
+                "             8oz self raising flour\n" +
+                "             1 egg\n" +
+                "             vanilla essence\n" +
+                "             4oz chocolate chips\n" +
+                "step 1 : Mix sugar, butter and self raising flour thoroughly\n" +
+                "step 2 : Stir in egg, a few drops of vanilla essence and chocolate chips.\n" +
+                "step 3 : Roll out mixture and cut out cookies.\n" +
+                "step 4 : Bake in hot oven (Gas Mark 6) for 10-12 minutes.");
+    }
+}
